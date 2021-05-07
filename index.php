0746ae04dfed9c82e8675d9aed3d96df5e4bef7d
gadiel <gadielven@gmail.com>
2021-05-07 11:58:53 -0500

    formulario y agregar el post

--- index.php
+++ index.php
@@ -1,7 +1,27 @@
 <?php
-
+/*
 include 'config/conexion.php';
 $db= new conexion();
-
-
+*/
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi agenda</title>
+</head>
+<body>
+    <form action="post.php" method="POST">
+    <input type="text" name="nombre" placeholder="nombre" required="requiered">
+    <input type="text" name="domicilio" placeholder="calle y numero" required="requiered">
+    <input type="text" name="telefono" placeholder="telefono" required="requiered">
+    <textarea name="comentarios" required="requiered">Comentarios</textarea>
+    <input type="hidden" name="accion" value="insert">
+    <input type="submit" name="submit" value="Enviar">
+
+    </form>
+</body>
+</html>
---
+++ post.php
@@ -0,0 +1,17 @@
+<?php
+
+if(isset($_POST['submit'])){
+
+    if($_POST['action']=='insert'){
+
+    }
+
+    if($_POST['action']=='update'){
+        
+    }
+
+}else{
+    header("Location: index.php");
+
+}
+?>
