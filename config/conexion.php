46c557d7fbac271941b130574bbf3b9df73239a6
gadiel <gadielven@gmail.com>
2021-05-07 11:44:57 -0500

    se creo la conexion a la base de datos

---
+++ config/conexion.php
@@ -0,0 +1,24 @@
+<?php
+
+class Conexion extends mysqli{
+    
+    private $DB_HOST = 'localhost';
+    private $DB_USER = 'root';
+    private $DB_PASS = 'mantequilla';
+    private $DB_NAME = 'miagenda';
+
+    public function __construct(){
+
+        parent:: __construct($this->DB_HOST,$this->DB_USER,$this->DB_PASS,$this->DB_NAME);
+
+        $this->set_charset('utf8');
+
+        $this->connect_errno ? die('Error al conectar'. mysqli_connect_errno()) : $m ='conectado';
+
+        echo $m;
+    }
+
+
+}
+
+?>
